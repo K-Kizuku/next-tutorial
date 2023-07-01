6312743a4d7192567ffade00a75dd603847f106e
K-Kizuku <kotani.kizuku@gmail.com>
2023-07-02 03:25:27 +0900

    [add] Octcatの実装

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+import Header from "@/components/Header";
+import Octcat from "@/components/Octcat";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${inter.className}`}>
+      <Header />
+      <Octcat />
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,31 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      keyframes: {
+        octocatWave: {
+          "0%": {
+            transform: "rotate(0)"
+          },
+          "20%": {
+            transform: "rotate(-25deg)"
+          },
+          "40%": {
+            transform: "rotate(10deg)"
+          },
+          "60%": {
+            transform: "rotate(-25deg)"
+          },
+          "80%": {
+            transform: "rotate(10deg)"
+          },
+          "100%": {
+            transform: "rotate(0)"
+          },
+        }
+      },
+      animation: {
+        octocatWave:"octocatWave 560ms ease-in-out"
+      }
     },
   },
   plugins: [],
